3e621f88db80c4b3440b4438f531bb1a17de0250
Chenguang Shen <pscggeforce@gmail.com>
2015-06-30 15:04:58 -0700

    show accelerometer sampling rate

--- iSensorDashboard WatchKit Extension/InterfaceController.swift
+++ iSensorDashboard WatchKit Extension/InterfaceController.swift
@@ -9,11 +9,18 @@
 import WatchKit
 import Foundation
 import CoreMotion
+// import HealthKit
 
 
 class InterfaceController: WKInterfaceController {
     
     let mCMMotionManager = CMMotionManager()
+    var accCount = 0
+    var startTs: NSTimeInterval?
+    var lastTS: NSTimeInterval?
+    
+//    let healthStore = HKHealthStore()
+//    let heartRateType = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)!
 
     @IBOutlet var labelAccX: WKInterfaceLabel!
     @IBOutlet var labelAccY: WKInterfaceLabel!
@@ -21,18 +28,35 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var labelGyroX: WKInterfaceLabel!
     @IBOutlet var labelGyroY: WKInterfaceLabel!
     @IBOutlet var labelGyroZ: WKInterfaceLabel!
-    
+    @IBOutlet var labelHeartRate: WKInterfaceLabel!
+    @IBOutlet var accRateLabel: WKInterfaceLabel!
+    @IBOutlet var accTSLabel: WKInterfaceLabel!
     
     @IBAction func startSensor() {
+        // Acc
+        self.accCount = 0
+        mCMMotionManager.accelerometerUpdateInterval = 0
         if (mCMMotionManager.accelerometerAvailable == true) {
             let handler:CMAccelerometerHandler = {(data: CMAccelerometerData?, error: NSError?) -> Void in
-                self.labelAccX.setText(String(format: "%.6f", data!.acceleration.x))
-                self.labelAccY.setText(String(format: "%.6f", data!.acceleration.y))
-                self.labelAccZ.setText(String(format: "%.6f", data!.acceleration.z))
+//                self.labelAccX.setText(String(format: "%.6f", data!.acceleration.x))
+//                self.labelAccY.setText(String(format: "%.6f", data!.acceleration.y))
+//                self.labelAccZ.setText(String(format: "%.6f", data!.acceleration.z))
+                if self.accCount == 0 {
+                    self.startTs = data?.timestamp
+                }
+                self.lastTS = data?.timestamp
+                self.accCount++
             }
             mCMMotionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: handler)
         }
+        else {
+            self.labelGyroX.setText("Acc N/A")
+            self.labelGyroY.setText("Acc N/A")
+            self.labelGyroZ.setText("Acc N/A")
+        }
         
+        // Gyro
+        mCMMotionManager.gyroUpdateInterval = 0
         if (mCMMotionManager.gyroAvailable == true) {
             let handler:CMGyroHandler = {(data: CMGyroData?, error: NSError?) -> Void in
                 self.labelGyroX.setText(String(format: "%.6f", data!.rotationRate.x))
@@ -41,12 +65,32 @@ class InterfaceController: WKInterfaceController {
             }
             mCMMotionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: handler)
         }
+        else {
+            self.labelGyroX.setText("Gyro N/A")
+            self.labelGyroY.setText("Gyro N/A")
+            self.labelGyroZ.setText("Gyro N/A")
+        }
+        
+//        // Heart rate
+//        if HKHealthStore.isHealthDataAvailable() != true {
+//            self.labelHeartRate.setText("HR not availabel")
+//        }
+//        
+//        let dataTypes = NSSet(object: heartRateType) as! Set<HKObjectType>
+//        healthStore.requestAuthorizationToShareTypes(nil, readTypes: dataTypes) { (success, error) -> Void in
+//            if success != true {
+//                self.labelHeartRate.setText("HR not allowed")
+//            }
+//        }
     }
     
     @IBAction func stopSensor() {
-        mCMMotionManager.stopGyroUpdates()
+        mCMMotionManager.stopAccelerometerUpdates()
         mCMMotionManager.stopGyroUpdates()
         
+        // let accFreq = self.accCount / (Int(self.lastTS!) % 60 - Int(self.startTs!) % 60)
+        accRateLabel.setText(String(format: "%d", self.accCount))
+        accTSLabel.setText(String(format: "%d", Int(self.lastTS!) - Int(self.startTs!)))
     }
     
     override func awakeWithContext(context: AnyObject?) {
